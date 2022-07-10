c245a266cf588ab5eae55856ff000a38406293d2
Robert <xiurobert@gmail.com>
2022-07-10 10:58:46 +0800

    Reformat code

--- approx/core/backends/common.py
+++ approx/core/backends/common.py
@@ -23,13 +23,16 @@ def auto_select_backend() -> BackendEngine:
         An instance of whatever backend is most appropriate.
     """
     # don't import the backends until it is actually selected
-    if 'torch' in sys.modules:
+    if "torch" in sys.modules:
         return set_backend(BackendChoice.TORCH)
-    raise BackendNotSupported(f"This backend is currently not supported. "
-                              f"Supported backends: {[b.name for b in BackendChoice]}")
+    raise BackendNotSupported(
+        f"This backend is currently not supported. "
+        f"Supported backends: {[b.name for b in BackendChoice]}"
+    )
 
 
 def set_backend(choice: BackendChoice) -> BackendEngine:
     if choice == BackendChoice.TORCH:
         from approx.core.backends.pytorch import PyTorchBackend
+
         return PyTorchBackend()
