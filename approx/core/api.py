9737a44ed1691685df9f9148aba0629d4e344001
Robert <xiurobert@gmail.com>
2022-07-10 10:06:55 +0800

    Some initial frameworks for the library

---
+++ approx/core/api.py
@@ -0,0 +1,27 @@
+"""
+Contains stuff to be exported in the public API.
+"""
+from contextlib import contextmanager
+
+from approx.core.backends.common import auto_select_backend
+
+
+@contextmanager
+def auto_cast_all() -> None:
+    """
+    Context manager that automatically casts and applies quantization to
+    any tensors created within the context.
+
+    Returns:
+        Context manager
+    """
+    pass
+
+
+def auto_set_backend() -> None:
+    """
+    Automatically selects an appropriate backend to utilize.
+    Returns:
+
+    """
+    auto_select_backend()
---
+++ approx/core/backends/pytorch.py
@@ -0,0 +1,7 @@
+import torch
+
+from approx.core.backends.common import BackendEngine
+
+
+class PyTorchBackend(BackendEngine):
+    pass
