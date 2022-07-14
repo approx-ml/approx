a6a42f9d71fdd0ab1de0260d8eb7d07f6d99ed65
Robert <xiurobert@gmail.com>
2022-07-14 14:50:52 +0800

    Build compare feature
    See: #20

--- approx/core/api.py
+++ approx/core/api.py
@@ -1,8 +1,11 @@
 """
 Contains stuff to be exported in the public API.
 """
+from typing import List, Optional, Callable, Any
+
 from approx.core import _vars
 from approx.core.backend.common import auto_select_backend
+from approx.core.compare import Metric, CompareResult, _CompareRunner
 from approx.core.device.common import auto_select_device
 
 
@@ -20,6 +23,34 @@ def auto_quantize(model, pretrained: bool = True):
         )
     qmodel = _vars._APPROX_BACKEND.auto_quantize(model, pretrained)
     return qmodel
+
+
+def compare(model, quantized_model, *,
+            eval_loop: Callable[[Any], List[List[float]]],
+            metrics: List[Metric]) -> CompareResult:
+    """
+    Compares your normal model with your quantized model
+
+    Args:
+        model: Your normal model
+        quantized_model: Your quantized model
+        eval_loop: Your evaluation loop that operates on your model and returns list of metric histories
+                    For example, if you select the LOSS metric only, your eval loop should return
+                    [loss_history] where loss_history is a list of the loss encountered per batch.
+                    If you select multiple metrics, the length of the returned list should be the same
+                    as the number of metrics you selected. For example, selecting LOSS and ACCURACY metrics should require
+                    your function return something like [loss_history, accuracy_history].
+        metrics: A list of metrics to compare. If not specified, defaults to LOSS.
+
+    Notes:
+        For certain backends, we support automatic generation of the eval loop given the
+        model metrics.
+
+    Returns:
+        Useful statistical information
+    """
+    runner = _CompareRunner([model, quantized_model], eval_loop, metrics)
+    return runner.run()
 
 
 def auto_cast_all(*args) -> None:
---
+++ approx/core/compare.py
@@ -0,0 +1,164 @@
+from typing import List, Any, Dict, Callable
+import statistics
+
+import enum
+
+
+class Metric(enum.Enum):
+    LOSS = 0
+    ACCURACY = 1
+
+
+class _EvalHistory:
+    def __init__(self, model: Any, metrics: List[Metric], histories: List[List[float]]):
+        assert len(metrics) == len(set(metrics)), "Metrics must be unique"
+        try:
+            str(model)
+        except Exception:
+            raise ValueError("Currently does not support models which do not have `__str__`")
+        self.model = model
+        self.metrics = metrics
+        self._histories = histories
+
+    def mean(self, metric: Metric) -> float:
+        r"""
+        Computes the mean (also known as the average)
+
+        $$
+        \frac{1}{N} \sum_{i=1}^N x_i
+        $$
+        where N is the number of batches
+
+        Args:
+            metric: The metric to compute the mean for
+
+        Returns:
+            The mean of that metric's history
+        """
+        # implementation note: this could be cached in case the history is very large
+        # and computing may take significant time
+        return statistics.fmean(self[metric])
+
+    def std(self, metric: Metric) -> float:
+        r"""
+        Computes the standard deviation of the metric history
+
+        $$
+        \sqrt{\frac{1}{N-1} \sum_{i=1}^N (x_i - \bar{x})^2}
+        $$
+        where N is the number of batches
+
+        Args:
+            metric: The metric to compute the standard deviation for
+
+        Returns:
+            The standard deviation of the metric history
+        """
+        return statistics.pstdev(self[metric])
+
+    def median(self, metric: Metric) -> float:
+        r"""
+        Computes the median of the metric history
+
+        $$
+        \frac{1}{2} \left[ \frac{1}{N} \sum_{i=1}^N x_i + \frac{1}{N} \sum_{i=1}^N x_i \right]
+        $$
+        where N is the number of batches
+
+        Args:
+            metric: The metric to compute the median for
+
+        Returns:
+            The median of the metric history
+        """
+        return statistics.median(self[metric])
+
+    def __getitem__(self, metric: Metric) -> List[float]:
+        """
+        Retrieves the metric history for some metric. Useful if you need
+        access to the actual history itself
+
+        Args:
+            metric: The metric to return the history for
+
+        Returns:
+            The history of the given metric
+        """
+        return self._histories[self.metrics.index(metric)]
+
+class CompareResult:
+    """
+    A result that is returned after calling `approx.compare`
+    """
+    def __init__(self, results: List[_EvalHistory], __private=False):
+        assert __private, "Please do not construct this manually"
+        self._results = results
+
+    def mean(self, metric: Metric) -> Dict[str, float]:
+        r"""
+        Computes the mean for some particular metric
+
+        $$
+        \frac{1}{N} \sum_{i=1}^N x_i
+        $$
+
+        where N is the number of batches
+
+        Args:
+            metric: The metric to compute the mean for
+
+        Returns:
+            A dictionary that maps your model to the mean of that metric's history
+        """
+        return {str(result.model): result.mean(metric) for result in self._results}
+
+    def std(self, metric: Metric) -> Dict[str, float]:
+        r"""
+        Compares the standard deviations for some particular metric
+
+        $$
+        \sqrt{\frac{1}{N-1} \sum_{i=1}^N (x_i - \bar{x})^2}
+        $$
+
+        where N is the number of batches
+
+        Args:
+            metric: The metric to compute the standard deviation for
+
+        Returns:
+            A dictionary that maps your model to the standard deviation of that metric's history
+        """
+        return {str(result.model): result.std(metric) for result in self._results}
+
+    def __str__(self):
+        # crude table printer, should be replaced with a more fancy one
+        ret_val = "-" * 60 + "\n"
+        ret_val += "|    Model    |    Metric   |    Mean    |    Std    |\n"
+        for result in self._results:
+            for metric in result.metrics:
+                ret_val += "| {} | {} | {} | {} |\n".format(
+                    str(result.model), metric.name.lower(),
+                    result.mean(metric), result.std(metric)
+                )
+        ret_val += "-" * 60
+        return ret_val
+
+
+class _CompareRunner:
+    """
+    Runs the compare operation
+    """
+    def __init__(self, models: List[Any],
+                 eval_loop: Callable[[Any], List[List[float]]], metrics: List[Metric]):
+        # todo: auto generate eval loop for certain backends
+        self._models = models
+        self._eval_loop = eval_loop
+        self._metrics = metrics
+        self._results = []
+
+    def run(self) -> CompareResult:
+        # todo: investigate multiprocessing
+        for model in self._models:
+            metric_hist = self._eval_loop(model)
+            self._results.append(_EvalHistory(model, self._metrics, metric_hist))
+        return CompareResult(self._results, __private=True)
