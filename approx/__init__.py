12f37f7578e48bd5d70e9f8fb936e862671e8a33
Robert <xiurobert@gmail.com>
2022-07-25 11:36:13 +0800

    Forgot to remove it from import

--- approx/__init__.py
+++ approx/__init__.py
@@ -1,14 +1,8 @@
 from approx.core._vars import backend, device
-from approx.core.api import (
-    auto_cast_all,
-    auto_quantize,
-    auto_set_backend,
-    auto_set_device,
-)
+from approx.core.api import auto_quantize, auto_set_backend, auto_set_device
 
 # Please keep this sorted
 __all__ = [
-    "auto_cast_all",
     "auto_quantize",
     "auto_set_backend",
     "auto_set_device",
